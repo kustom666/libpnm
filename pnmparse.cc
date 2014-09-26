60b39535f59f50de044ff102571eb2de2d8576a0
Paul Forti <paul.forti@gmail.com>
2014-09-26 14:57:38 +0200

    Ajout de la bdd, crea et feed

--- pnmparse.cc
+++ pnmparse.cc
@@ -3,10 +3,20 @@
 #include <dirent.h>
 #include <sqlite3.h>
 
+static int callbackPrint(void *NotUsed, int argc, char **argv, char **azColName){
+	int i;
+	for(i=0; i<argc; i++){
+	  printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
+	printf("\n");
+	return 0;
+}
+
 int main(int argc, char const *argv[])
 {
 	int dbHandle;
 	sqlite3 *db;
+	char* errMsg = 0;
 	dbHandle = sqlite3_open("dbImages.sqlite", &db);
 	if(dbHandle)
 	{
@@ -15,6 +25,27 @@ int main(int argc, char const *argv[])
 		return 1;
 	}
 
+	dbHandle = sqlite3_exec(db, "CREATE TABLE Metadata(id int, type varchar(255), moyenne_rouge int, moyenne_vert int, moyenne_bleu int, nombre_contour int, moyenne_gradient int, PRIMARY KEY (id));",
+					  callbackPrint, 0, &errMsg);
+
+	if(dbHandle != SQLITE_OK)
+	{
+		fprintf(stderr, "Erreur sql : %s\n", errMsg);
+		sqlite3_free(errMsg);
+		return 1;	
+	}
+
+	dbHandle = sqlite3_exec(db, "CREATE TABLE Images(id int, nom varchar(255), hauteur int, largeur int, id_metadata int, PRIMARY KEY (id), FOREIGN KEY (id_metadata) REFERENCES Metadata(id));",
+					  callbackPrint, 0, &errMsg);
+
+	if(dbHandle != SQLITE_OK)
+	{
+		fprintf(stderr, "Erreur sql : %s\n", errMsg);
+		sqlite3_free(errMsg);
+		return 1;	
+	}
+
+	int idImage = 0;
 	DIR *dir;
 	struct dirent *ent;
 	if ((dir = opendir (".")) != NULL) {
@@ -37,9 +68,21 @@ int main(int argc, char const *argv[])
 	  				last++;
 	  			}
 	  		}
-	  		if(ent->d_name[last-2] == 'p' && ent->d_name[last-1] == 'n' && ent->d_name[last] == 'm')
+	  		if(ent->d_name[last-2] == 'p' && (ent->d_name[last-1] == 'n' || ent->d_name[last-1] == 'g') && ent->d_name[last] == 'm')
 	  		{
+	  			PnmImage im;
+	  			im.loadFromFile(ent->d_name);
 	    		printf ("%s\n", ent->d_name);
+	    		char requete[2048];
+	    		sprintf(requete, "INSERT INTO Images(id, nom, hauteur, largeur) VALUES(%d,\'%s\', %d, %d)", idImage, ent->d_name, im.getMHeight(), im.getMWidth());
+	    		dbHandle = sqlite3_exec(db, requete, callbackPrint, 0, &errMsg);
+				if(dbHandle != SQLITE_OK)
+				{
+					fprintf(stderr, "Erreur sql : %s\n", errMsg);
+					sqlite3_free(errMsg);
+					return 1;	
+				}
+				idImage ++;
 	    	}
 	    }
 	  }
@@ -49,5 +92,6 @@ int main(int argc, char const *argv[])
 	  perror ("Impossible d'ouvrir le repertoire courant\n");
 	  return EXIT_FAILURE;
 	}
+	sqlite3_close(db);
 	return 0;
 }
